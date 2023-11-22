87bd61d242f93d081a6612f6296e63a9c87310ae
gm50x <getuliomagela@outlook.com>
2023-11-22 20:28:27 -0300

    normalize event names propagated to rabbitmq as routing keys

--- src/sdk/amqp/amqp-event.propagator.ts
+++ src/sdk/amqp/amqp-event.propagator.ts
@@ -26,9 +26,25 @@ export class AmqpEventPropagator implements OnModuleInit, OnModuleDestroy {
     this.subscription.unsubscribe();
   }
 
+  private translateEventNameToRoutingKey(event: IEvent): string {
+    const baseName = event.constructor.name;
+    const baseNameWithoutSuffix = baseName.replace(/Event$/g, '');
+    const values = [baseNameWithoutSuffix[0].toLowerCase()];
+    for (let i = 1; i < baseNameWithoutSuffix.length; i++) {
+      const thisCharacter = baseNameWithoutSuffix[i];
+      if (thisCharacter === thisCharacter.toUpperCase()) {
+        values.push('.', thisCharacter.toLowerCase());
+      } else {
+        values.push(thisCharacter);
+      }
+    }
+    return values.join('');
+  }
+
   private async propagate(event: IEvent) {
     this.logger.debug('Propagating event');
-    await this.amqp.publish('events', event.constructor.name, event);
+    const eventName = this.translateEventNameToRoutingKey(event);
+    await this.amqp.publish('events', eventName, event);
     this.logger.log('Event Propagated');
   }
 }
