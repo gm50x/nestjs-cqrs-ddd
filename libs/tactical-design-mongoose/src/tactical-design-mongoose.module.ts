cd5788cb727d30fa5a2e06d36efd31461dd82cbf
gm50x <getuliomagela@outlook.com>
2024-03-06 20:34:51 -0300

    cleanup

--- libs/tactical-design-mongoose/src/tactical-design-mongoose.module.ts
+++
@@ -1,8 +0,0 @@
-import { Module } from '@nestjs/common';
-import { TacticalDesignMongooseService } from './tactical-design-mongoose.service';
-
-@Module({
-  providers: [TacticalDesignMongooseService],
-  exports: [TacticalDesignMongooseService],
-})
-export class TacticalDesignMongooseModule {}
